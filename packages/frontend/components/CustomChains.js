eb2e785364012a32aebc183ca7f00bd1b36ea035
pryority <matthewapryor@gmail.com>
2022-12-03 12:32:55 -0500

    fix prettier warning

--- packages/frontend/components/CustomChains.js
+++ packages/frontend/components/CustomChains.js
@@ -1,36 +1,36 @@
 const Gnosis = {
   id: 100,
-  name: 'Gnosis',
-  network: 'Gnosis',
+  name: "Gnosis",
+  network: "Gnosis",
   nativeCurrency: {
     decimals: 18,
-    name: 'xDai',
-    symbol: 'xDai',
+    name: "xDai",
+    symbol: "xDai",
   },
   rpcUrls: {
-    default: 'https://rpc.ankr.com/gnosis',
+    default: "https://rpc.ankr.com/gnosis",
   },
   blockExplorers: {
-    default: { name: 'Blockscout', url: 'https://blockscout.com/xdai/mainnet' },
+    default: { name: "Blockscout", url: "https://blockscout.com/xdai/mainnet" },
   },
-  iconUrl: 'https://images.prismic.io/koinly-marketing/16d1deb7-e71f-48a5-9ee7-83eb0f7038e4_Gnosis+Chain+Logo.png',
+  iconUrl: "https://images.prismic.io/koinly-marketing/16d1deb7-e71f-48a5-9ee7-83eb0f7038e4_Gnosis+Chain+Logo.png",
   testnet: false,
 };
 
 const Avalanche = {
   id: 43_114,
-  name: 'Avalanche',
-  network: 'avalanche',
+  name: "Avalanche",
+  network: "avalanche",
   nativeCurrency: {
     decimals: 18,
-    name: 'Avalanche',
-    symbol: 'AVAX',
+    name: "Avalanche",
+    symbol: "AVAX",
   },
   rpcUrls: {
-    default: 'https://api.avax.network/ext/bc/C/rpc',
+    default: "https://api.avax.network/ext/bc/C/rpc",
   },
   blockExplorers: {
-    default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+    default: { name: "SnowTrace", url: "https://snowtrace.io" },
   },
   testnet: false,
 };
